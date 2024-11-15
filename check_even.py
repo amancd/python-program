9b2a3a9c46b79678326d6bc50ac42b2f784e80d7
Kumar Raunak <163028047+SatanS-CL4W@users.noreply.github.com>
2024-11-15 11:25:19 +0530

    Update check_even.py

--- check_even.py
+++ check_even.py
@@ -4,8 +4,8 @@ n = int(input())
 
 def check_even_odd(n): 
   if n%2==0:
-    return "Even"  #If the number is divisible by 2 it is even
+    return "Even Number"  #If the number is divisible by 2 it is even
   else:
-    return "Odd"
+    return "Odd Number"
 
 print(check_even_odd(n)); #n would be better option
